83c19398aaec6e69e421a2225ff33b58078838f2
Josue Morales <dev.carlos.morales@gmail.com>
2024-04-20 18:50:15 -0500

    feat - header change styles

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, ViewEncapsulation, inject } from '@angular/core';
 import { LayoutService } from '@shared/data-access/layout/layout.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  encapsulation: ViewEncapsulation.None,
   standalone: true,
   imports: [],
+  host: { class: 'header' },
 })
 export class HeaderComponent {
   private _layoutService = inject(LayoutService);
